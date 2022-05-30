e954da83bf5a96022bfccd958cc55134a928bb73
ser888000 <cto8@ya.ru>
2022-05-30 23:24:46 +0300

    add comments

--- test_main_page.py
+++ test_main_page.py
@@ -7,12 +7,14 @@ link = "http://selenium1py.pythonanywhere.com/"
 
 @pytest.mark.skip
 def test_guest_should_see_login_link(browser):
+    # на странице есть ссылка на логин
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 @pytest.mark.skip    
 def test_guest_can_go_to_login_page(browser):
+    # переход на страницу с логином
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
@@ -21,6 +23,7 @@ def test_guest_can_go_to_login_page(browser):
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    # переход на страницу корзины + проверка на ее пустоту
     page = MainPage(browser, link)   
     page.open()
     page.go_to_basket_page()
